12875eb275a252e05b6ae802f30a722cf59661e5
Alexandre Deluc <alexandre.deluc07@gmail.com>
2022-01-24 14:15:17 +0100

    appinit

---
+++ app/src/main/java/fr/isen/deluc/androiderestaurant/homeActivity.kt
@@ -0,0 +1,14 @@
+package fr.isen.deluc.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class homeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
